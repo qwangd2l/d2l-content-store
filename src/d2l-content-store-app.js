70578db0664a040538185a99f671f9883f81c457
Quan Hui Wang <quanhui.wang@d2l.com>
2020-02-10 16:35:02 -0500

    Two-column layout component. Manage page. Fix up some issues with routing for multiple items e.g. one/two/three/four. Remove some-other-page placeholder page. Add language imports in the localize mixin. Add RTL mixin to page-view-element. Add a test file for manage page.

--- src/d2l-content-store-app.js
+++ src/d2l-content-store-app.js
@@ -2,14 +2,16 @@ import { css, html, LitElement } from 'lit-element/lit-element.js';
 import page from 'page/page.mjs';
 
 import { InternalLocalizeMixin } from './mixins/internal-localize-mixin.js';
+import { NavigationMixin } from './mixins/navigation-mixin.js';
 
-const BASE_PATH = '/d2l/contentstore/';
+const BASE_PATH = '/d2l/contentstore';
 const stripBasePath = path => path.replace(/^\/(d2l\/contentstore\/)?/, '');
 
-class D2lContentStoreApp extends InternalLocalizeMixin(LitElement) {
+class D2lContentStoreApp extends NavigationMixin(InternalLocalizeMixin(LitElement)) {
 	static get properties() {
 		return {
 			_page: { type: String },
+			_subView: { type: String },
 			prop1: { type: String },
 			someStringAttribute: { type: String },
 			someBooleanAttribute: { type: Boolean }
@@ -44,21 +46,32 @@ class D2lContentStoreApp extends InternalLocalizeMixin(LitElement) {
 	_setupPageNavigation() {
 		page.base(BASE_PATH);
 
-		page('404', this.setupPage.bind(this));
-		page('some-other-page', this.setupPage.bind(this));
-		page('my-objects', this.setupPage.bind(this));
+		page('/404', this.setupPage.bind(this));
+		page('/my-objects', this.setupPage.bind(this));
+		page('/manage/:subview/', this.setupPage.bind(this));
+		page('', this.setupPage.bind(this));
+		page('*', this.setupPage.bind(this));
 		page();
 	}
 
 	setupPage(ctx) {
-		let page = ctx && ctx.pathname && stripBasePath(ctx.pathname);
+		const pathNameWithoutBase = ctx && ctx.pathname && stripBasePath(ctx.pathname);
+		let page = pathNameWithoutBase.includes('/') ?
+			pathNameWithoutBase.slice(0, pathNameWithoutBase.indexOf('/')) :
+			pathNameWithoutBase;
+		const subView = pathNameWithoutBase.includes('/') ?
+			pathNameWithoutBase.slice(pathNameWithoutBase.indexOf('/') + 1) :
+			'';
 		switch (page) {
 			/* eslint-disable no-unused-expressions */
+			case '':
+				this._navigate('/manage/');
+				return;
+			case 'manage':
+				import('./d2l-content-store-manage.js');
+				break;
 			case 'my-objects':
 				import('./my-objects.js');
-				break;
-			case 'some-other-page':
-				import('./some-other-page.js');
 				break;
 			default:
 				page = '404';
@@ -67,16 +80,21 @@ class D2lContentStoreApp extends InternalLocalizeMixin(LitElement) {
 			/* eslint-enable no-unused-expressions */
 		}
 
+		this._subView = subView;
 		this._page = page;
 	}
 
 	render() {
 		return html`
-			Welcome to the content store.
-			<a href="/d2l/contentstore/404">click to go to 404</a>
+		<main id="main" role="main">
 			<my-objects ?active=${this._page === 'my-objects'} class="page"></my-objects>
-			<some-other-page class="page" ?active=${this._page === 'some-other-page'}></some-other-page>
+			<d2l-content-store-manage
+				class="page"
+				?active=${this._page === 'manage'}
+				subView=${this._subView}>
+			</d2l-content-store-manage>
 			<d2l-content-store-404 class="page" ?active=${this._page === '404'}></d2l-content-store-404>
+		</main>
 		`;
 	}
 }
