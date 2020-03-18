7bb1ee93c0497de970114d512b4a0cbdfc65f2ec
Quan Hui Wang <quanhui.wang@d2l.com>
2020-03-18 10:27:18 -0400

    Fix content-list test to accept a copy of testContentItems.

--- src/components/content-list/test/content-list.test.js
+++ src/components/content-list/test/content-list.test.js
@@ -55,7 +55,7 @@ describe('content-list', () => {
 
 	it('uploading content item does not appear if there is a next page and should be last item', async() => {
 		el.loading = false;
-		el.contentItems = testContentItems;
+		el.contentItems = testContentItems.slice();
 		el.hasNextPage = true;
 		el.sortQuery = 'updatedAt:asc';
 		await el.updateComplete;
@@ -78,7 +78,7 @@ describe('content-list', () => {
 	it('uploading content item with sort on updatedAt:asc', async() => {
 		el.loading = false;
 		el.sortQuery = 'updatedAt:asc';
-		el.contentItems = testContentItems;
+		el.contentItems = testContentItems.slice();
 		await el.updateComplete;
 
 		const now = new Date();
@@ -99,7 +99,7 @@ describe('content-list', () => {
 	it('uploading content item with sort on updatedAt:desc', async() => {
 		el.loading = false;
 		el.sortQuery = 'updatedAt:desc';
-		el.contentItems = testContentItems;
+		el.contentItems = testContentItems.slice();
 		await el.updateComplete;
 
 		const now = new Date();
